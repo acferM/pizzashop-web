8536bd47f2e30499597f5a3a80e01a537de68b9d
Matheus Acfer <promacfer@gmail.com>
2024-03-12 17:17:22 -0300

    app until order cancelation

---
+++ src/pages/app/orders/orders.tsx
@@ -0,0 +1,90 @@
+import { useQuery } from '@tanstack/react-query'
+import { Helmet } from 'react-helmet-async'
+import { useSearchParams } from 'react-router-dom'
+import { z } from 'zod'
+
+import { getOrders } from '@/api/get-orders'
+import { Pagination } from '@/components/pagination'
+import {
+  Table,
+  TableBody,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+
+import { OrderTableFilters } from './order-table-filters'
+import { OrderTableRow } from './order-table-row'
+
+export function Orders() {
+  const [searchParams, setSearchParams] = useSearchParams()
+
+  const filters = {
+    orderId: searchParams.get('orderId'),
+    customerName: searchParams.get('customerName'),
+    status: searchParams.get('status'),
+    pageIndex: z.coerce
+      .number()
+      .transform((page) => page - 1)
+      .parse(searchParams.get('page') ?? '1'),
+  }
+
+  const { data: result } = useQuery({
+    queryKey: ['orders', filters],
+    queryFn: () => getOrders(filters),
+  })
+
+  function handlePaginate(page: number) {
+    setSearchParams((state) => {
+      state.set('page', (page + 1).toString())
+
+      return state
+    })
+  }
+
+  return (
+    <>
+      <Helmet title="Pedidos" />
+
+      <div className="flex flex-col gap-4">
+        <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
+        <div className="space-y-2.5">
+          <OrderTableFilters />
+
+          <div className="rounded-md border">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="w-[64px]"></TableHead>
+                  <TableHead className="w-[140px]">Identificador</TableHead>
+                  <TableHead className="w-[180px]">Realizado há</TableHead>
+                  <TableHead className="w-[140px]">Status</TableHead>
+                  <TableHead>Cliente</TableHead>
+                  <TableHead className="w-[140px]">Total do pedido</TableHead>
+                  <TableHead className="w-[164px]"></TableHead>
+                  <TableHead className="w-[132px]"></TableHead>
+                </TableRow>
+              </TableHeader>
+
+              <TableBody>
+                {result &&
+                  result.orders.map((order) => (
+                    <OrderTableRow key={order.orderId} order={order} />
+                  ))}
+              </TableBody>
+            </Table>
+          </div>
+
+          {result && (
+            <Pagination
+              pageIndex={result.meta.pageIndex}
+              perPage={result.meta.perPage}
+              totalCount={result.meta.totalCount}
+              onPageChange={handlePaginate}
+            />
+          )}
+        </div>
+      </div>
+    </>
+  )
+}
